7d574d2d89371fd6cd2e4d15dca6bd948792f8c2
Saadiq Mohiuddin <saadiq.mohiuddin@gmail.com>
2021-07-29 12:50:51 -0600

    ga-dlcd updates

--- src/stactools/ga_dlcd/__init__.py
+++ src/stactools/ga_dlcd/__init__.py
@@ -1,1 +1,16 @@
-__version__ = "0.2.0"
+import stactools.core
+from stactools.ga_dlcd.stac import create_collection, create_item
+from stactools.ga_dlcd.cog import create_cog
+
+__all__ = [create_collection, create_item, create_cog]
+
+stactools.core.use_fsspec()
+
+
+def register_plugin(registry):
+    from stactools.ga_dlcd import commands
+    registry.register_subcommand(commands.create_gadlcd_command)
+
+
+__version__ = '0.2.3'
+"""Library version"""
---
+++ tests/test_commands.py
@@ -0,0 +1,78 @@
+import os.path
+from tempfile import TemporaryDirectory
+import logging
+import pystac
+
+from stactools.ga_dlcd.commands import create_gadlcd_command
+from stactools.testing import CliTestCase
+
+from tests import test_data
+logger = logging.getLogger(__name__)
+
+class CreateCollectionTest(CliTestCase):
+    def create_subcommand_functions(self):
+        return [create_gadlcd_command]
+
+    def test_create_collection(self):
+        with TemporaryDirectory() as tmp_dir:
+            json_path = os.path.join(tmp_dir, "test.json")
+            result = self.run_command(
+                ["ga-dlcd", "create-collection", "-d", json_path])
+            self.assertEqual(result.exit_code,
+                             0,
+                             msg="\n{}".format(result.output))
+
+            jsons = [p for p in os.listdir(tmp_dir) if p.endswith(".json")]
+            self.assertEqual(len(jsons), 1)
+
+            collection = pystac.read_file(os.path.join(tmp_dir, jsons[0]))
+
+            collection.validate()
+
+    def test_create_cog(self):
+        with TemporaryDirectory() as tmp_dir:
+            test_path = test_data.get_path("data-files")
+            paths = [
+                os.path.join(test_path, d) for d in os.listdir(test_path)
+                if d.lower().endswith(".tif")
+            ]
+
+            for path in paths:
+
+                result = self.run_command([
+                    "ga-dlcd", "create-cog", "-d", tmp_dir, "-s", path
+                ])
+                self.assertEqual(result.exit_code,
+                                 0,
+                                 msg="\n{}".format(result.output))
+
+                cogs = [
+                    p for p in os.listdir(tmp_dir) if p.endswith("_cog.tif")
+                ]
+                self.assertEqual(len(cogs), 1)
+
+    def test_create_item(self):
+        with TemporaryDirectory() as tmp_dir:
+
+            test_path = test_data.get_path("data-files")
+            paths = [
+                os.path.join(test_path, d) for d in os.listdir(test_path)
+                if d.lower().endswith(".tif")
+            ]
+
+            for path in paths:
+                result = self.run_command([
+                        "ga-dlcd", "create-item", "-d", tmp_dir, "-c", path
+                    ])
+                self.assertEqual(result.exit_code,
+                                0,
+                                msg="\n{}".format(result.output))
+
+                jsons = [p for p in os.listdir(tmp_dir) if p.endswith(".json")]
+                self.assertEqual(len(jsons), 1)
+
+                item_path = os.path.join(tmp_dir, jsons[0])
+
+                item = pystac.read_file(item_path)
+
+        item.validate()
