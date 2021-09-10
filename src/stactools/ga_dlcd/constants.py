d6629d705fecc39474244628c8d0822a75543d6d
Tyler Battle <tyler@battle.ca>
2021-09-09 23:35:19 -0700

    Fix Classification values and add colormap

--- src/stactools/ga_dlcd/constants.py
+++ src/stactools/ga_dlcd/constants.py
@@ -25,3 +25,57 @@ GADLCD_PROVIDER = Provider(
 GADLCD_BOUNDING_BOX = [96.00, -44.00, 168.00, -9.00]
 GADLCD_START_YEAR = '2001'
 GADLCD_END_YEAR = '2015'
+
+NO_DATA_VALUE = 0
+
+CLASSIFICATION_VALUES = {
+    0: "No Data",
+    2: "Mines and Quarries",
+    35: "Urban Areas",
+    3: "Lakes and dams",
+    4: "Salt lakes",
+    5: "Irrigated cropping",
+    8: "Rain fed cropping",
+    6: "Irrigated pasture",
+    9: "Rain fed pasture",
+    7: "Irrigated sugar",
+    10: "Rain fed sugar",
+    11: "Wetlands",
+    15: "Alpine meadows",
+    16: "Open Hummock Grassland",
+    14: "Closed Tussock Grassland",
+    18: "Open Tussock Grassland",
+    19: "Scattered shrubs and grasses",
+    24: "Dense Shrubland",
+    25: "Open Shrubland",
+    31: "Closed Forest",
+    32: "Open Forest",
+    34: "Woodland",
+    33: "Open Woodland",
+}
+
+COLOUR_MAP = {
+    0: (0, 0, 0, 0),
+    1: (130, 130, 130, 255),
+    35: (200, 200, 200, 255),
+    3: (0, 70, 173, 255),
+    4: (150, 225, 255, 255),
+    5: (90, 36, 90, 255),
+    8: (198, 141, 153, 255),
+    6: (166, 38, 170, 255),
+    9: (226, 194, 199, 255),
+    7: (183, 18, 52, 255),
+    10: (219, 77, 105, 255),
+    11: (0, 178, 160, 255),
+    15: (255, 255, 255, 255),
+    16: (255, 255, 115, 255),
+    14: (255, 121, 0, 255),
+    18: (255, 169, 82, 255),
+    19: (255, 255, 190, 255),
+    24: (175, 136, 80, 255),
+    25: (193, 168, 117, 255),
+    31: (0, 133, 0, 255),
+    32: (20, 194, 0, 255),
+    34: (186, 232, 96, 255),
+    33: (214, 255, 138, 255),
+}
